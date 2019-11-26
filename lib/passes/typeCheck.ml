fcd3c4542dd508290b3582b53022291ddbb04880
Niklas Vangerow <n@vangerow.net>
2019-11-26 15:59:53 +0000

    reorganize to allow passes

---
+++ lib/passes/typeCheck.ml
@@ -0,0 +1,29 @@
+
+module Walker_TypeCheck = Walker.Make(struct 
+    let visit_program_pre p = p
+    let visit_program_pos p = p
+
+    let visit_block_pre b = b
+    let visit_block_pos b = b
+
+    let visit_stmt_pre s = s
+    let visit_stmt_pos s = s
+
+    let visit_decl_pre d = d
+    let visit_decl_pos d = d
+
+    let visit_binop b = b
+
+    let visit_unop u = u
+
+    let visit_value v = v
+
+    let visit_expr_pre e = e
+    let visit_expr_pos e = e
+
+    let visit_loc_pre l = l
+    let visit_loc_pos l = l
+
+    let visit_typ_pre t = t
+    let visit_typ_pos t = t
+  end)
