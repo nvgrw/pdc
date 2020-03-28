1461cd99af51066b3da27d3dce4d4952f8e85668
Niklas Vangerow <n@vangerow.net>
2020-03-28 14:22:51 +0000

    combine error string generation

--- lib/compile.ml
+++ lib/compile.ml
@@ -32,64 +32,61 @@ let generate_context get_lines s_pos e_pos =
   let fname = match s_pos.pos_fname with | "" -> "<no file>" | _ -> s_pos.pos_fname in
   sprintf "%s\n%s:%d:%d;%d:%d" code fname s_pos.pos_lnum (s_off + 1)  e_pos.pos_lnum (e_off + 1)
 
+let error_string err get_lines =
+  match err with
+  | TypeError (IncompatibleBinOp (expr, lt, op, rt)) -> 
+    let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: %s incompatible with types %s and %s." context (show_binop pp_meta op) (show_typ pp_meta lt) (show_typ pp_meta rt) 
+  | TypeError (IncompatibleUnOp (expr, op, t)) -> 
+    let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: %s incompatible with type %s." context (show_unop pp_meta op) (show_typ pp_meta t)
+  | TypeError (IncompatibleAssignment (stmt, ltyp, typ)) ->
+    let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: incompatible assignment between types %s and %s." context (show_typ pp_meta ltyp) (show_typ pp_meta typ)
+  | TypeError (IfRequiresBoolean stmt)->
+    let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: if statement requires condition to evaluate to boolean expression." context
+  | TypeError (WhileRequiresBoolean stmt) ->
+    let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: while statement requires condition to evaluate to boolean expression." context
+  | TypeError (DoRequiresBoolean stmt) ->
+    let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: do statement requires condition to evaluate to a boolean expression." context
+  | TypeError (UntypedSubExpressions expr) ->
+    let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: untyped subexpressions in expression %s." context (show_expr pp_meta expr)
+  | TypeError (UntypedSubLocations loc) ->
+    let context = (match get_meta_loc loc with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: untyped sublocations in location %s." context (show_loc pp_meta loc)
+  | TypeError (UntypedStatementFragment stmt) ->
+    let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: untyped statement fragment %s." context (show_stmt pp_meta stmt)
+  | StructuralError (BadIdentifier (m, ident)) -> 
+    let context = (match m with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: identifier `%s' not declared in scope." context ident
+  | StructuralError (DuplicateIdentifier (decl, ident)) -> 
+    let context = (match get_meta_decl decl with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: identifier `%s' already declared in scope." context ident
+  | CodegenError (CannotGenerateExpression expr) ->
+    let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: cannot generate expression %s." context (show_expr pp_meta expr)
+  | CodegenError (NegateOnlyIntOrFloat expr) ->
+    let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
+    sprintf "%s: can only negate int or float %s." context (show_expr pp_meta expr)
+  | Message m -> m
+
 let generate (p: meta program) (get_lines: int -> int -> string list) = 
   let post_semant = Semant.check p
-  in match post_semant with
-  | Error err -> print_endline @@ begin
-      match err with
-      | TypeError (IncompatibleBinOp (expr, lt, op, rt)) -> 
-        let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: %s incompatible with types %s and %s." context (show_binop pp_meta op) (show_typ pp_meta lt) (show_typ pp_meta rt) 
-      | TypeError (IncompatibleUnOp (expr, op, t)) -> 
-        let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: %s incompatible with type %s." context (show_unop pp_meta op) (show_typ pp_meta t)
-      | TypeError (IncompatibleAssignment (stmt, ltyp, typ)) ->
-        let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: incompatible assignment between types %s and %s." context (show_typ pp_meta ltyp) (show_typ pp_meta typ)
-      | TypeError (IfRequiresBoolean stmt)->
-        let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: if statement requires condition to evaluate to boolean expression." context
-      | TypeError (WhileRequiresBoolean stmt) ->
-        let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: while statement requires condition to evaluate to boolean expression." context
-      | TypeError (DoRequiresBoolean stmt) ->
-        let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: do statement requires condition to evaluate to a boolean expression." context
-      | TypeError (UntypedSubExpressions expr) ->
-        let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: untyped subexpressions in expression %s." context (show_expr pp_meta expr)
-      | TypeError (UntypedSubLocations loc) ->
-        let context = (match get_meta_loc loc with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: untyped sublocations in location %s." context (show_loc pp_meta loc)
-      | TypeError (UntypedStatementFragment stmt) ->
-        let context = (match get_meta_stmt stmt with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: untyped statement fragment %s." context (show_stmt pp_meta stmt)
-      | StructuralError (BadIdentifier (m, ident)) -> 
-        let context = (match m with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: identifier `%s' not declared in scope." context ident
-      | StructuralError (DuplicateIdentifier (decl, ident)) -> 
-        let context = (match get_meta_decl decl with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: identifier `%s' already declared in scope." context ident
-      | CodegenError (CannotGenerateExpression expr) ->
-        let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: cannot generate expression %s." context (show_expr pp_meta expr)
-      | CodegenError (NegateOnlyIntOrFloat expr) ->
-        let context = (match get_meta_expr expr with Position (s_pos, e_pos) -> generate_context get_lines s_pos e_pos) in
-        sprintf "%s: can only negate int or float %s." context (show_expr pp_meta expr)
-      | Message m -> m
-    end
-  | Success (_, semant_ast) -> 
-    let post_gen = Gen.generate semant_ast
-    in begin 
-      match post_gen with
-      | Error err -> print_endline @@ begin 
-          match err with
-          | Message m -> m
-          | _ -> "no message"
-        end
-      | Success (_, gen_ast) ->
-        print_endline @@ show_program pp_meta gen_ast 
-    end
+  in begin match post_semant with
+    | Error err -> print_endline @@ error_string err get_lines
+    | Success (_, semant_ast) -> 
+      let post_gen = Gen.generate semant_ast
+      in begin match post_gen with
+        | Error err -> print_endline @@ error_string err get_lines
+        | Success (_, gen_ast) ->
+          print_endline @@ show_program pp_meta gen_ast 
+      end
+  end
 
 let compile buf get_lines = 
   try generate (Parser.program Lexer.token buf) get_lines with
