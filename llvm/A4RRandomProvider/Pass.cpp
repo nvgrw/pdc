3ba1a40f46a57372fa65116076d0a09c55d50814
Niklas Vangerow <n@vangerow.net>
2020-04-21 23:27:10 +0100

    trying to figure out some way to hook into the codegen

--- llvm/A4RRandomProvider/Pass.cpp
+++ llvm/A4RRandomProvider/Pass.cpp
@@ -1,9 +1,51 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/PassSupport.h"
+#include "llvm/CodeGen/ChooseLowering.h"
+#include "llvm/InitializePasses.h"
 
-#include "llvm/IR/PassManager.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 using namespace llvm;
 
 namespace {
-struct A4RandomProviderPass : public PassInfoMixin<A4RandomProviderPass> {
+struct A4RRandomProvider : public FunctionPass {
+  static char ID;
+
+  A4RRandomProvider() : FunctionPass(ID) {
+    initializeCodeGen(*PassRegistry::getPassRegistry());
+  }
+  StringRef getPassName() const override;
+  void getAnalysisUsage(AnalysisUsage &AU) const override;
+
+  bool doInitialization(Module &M) override;
+  bool runOnFunction(Function &F) override;
 };
+} // anonymous namespace
+
+char A4RRandomProvider::ID = 0;
+
+StringRef A4RRandomProvider::getPassName() const {
+  return "HELLO HELLO HELLO HELLO HELLO";
 }
+
+void A4RRandomProvider::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.addRequired<ChooseLowering>();
+}
+
+bool A4RRandomProvider::doInitialization(Module &M) {
+  return false;
+}
+
+bool A4RRandomProvider::runOnFunction(Function &F) {
+  errs() << "Hello: ";
+  errs().write_escaped(F.getName()) << '\n';
+  return false;
+}
+
+static llvm::RegisterStandardPasses Y(
+    llvm::PassManagerBuilder::EP_EarlyAsPossible,
+    [](const llvm::PassManagerBuilder &Builder,
+       llvm::legacy::PassManagerBase &PM) { PM.add(new A4RRandomProvider()); });
